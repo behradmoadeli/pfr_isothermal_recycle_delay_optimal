1921deb547554f8dafdd482686475519b1ade611
Behrad 'Brad' Moadeil <moadeli@ualberta.ca>
2023-08-30 13:45:39 -0600

    improved init file with python version < 3.11

--- functions/__init__.py
+++ functions/__init__.py
@@ -10,4 +10,8 @@ from .find_eig import find_eig
 from .save_dataframe_to_csv import save_dataframe_to_csv
 from .create_label import create_label
 from .plot_single_df import plot_single_df
-# from .plot_multiple_datasets import plot_multiple_datasets
+from sys import version_info
+
+# Check if Python version is 3.11 or higher
+if version_info >= (3, 11):
+    from .plot_multiple_datasets import plot_multiple_datasets
