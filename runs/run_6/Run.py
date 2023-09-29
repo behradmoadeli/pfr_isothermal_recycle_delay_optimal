a48ad6b7116ce3f63c768fffd9bd0db9ed7907d4
behradmoadeli <moadeli@ualberta.ca>
2023-09-28 19:43:17 -0600

    finished run_6 on server
    blank

--- runs/run_6/Run.py
+++ runs/run_6/Run.py
@@ -7,16 +7,18 @@ working_directory = set_directory(__file__)
 # Prepare kwargs for find_eig() funcition
 guess = {
     'guess_range_real':[-275,24,150],
-    'guess_range_imag':[0,500,50]
+    'guess_range_imag':[0,100,50]
 }
 
 # Get default_pars and their coresponding solution
 default_pars = obtain_default_pars('pars_list.csv')
+print("Working on default set")
 save_dataframe_to_csv(*find_eig(default_pars, **guess, round_sig_digits=3), 'CSV')
 
 # Get the solutions for parameter variation
 pars_list = create_custom_pars_list('pars_list.csv')
 for par in pars_list:
+    print("Working on a new custom set")
     save_dataframe_to_csv(*find_eig(par, default_pars=default_pars, **guess), 'CSV')
 
 # df = plot_single_df('CSV/default.csv')[0]
