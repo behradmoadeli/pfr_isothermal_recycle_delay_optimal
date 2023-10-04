9b967deaf17c32e72078ae82fc834b2cc06d19c7
Behrad 'Brad' Moadeil <moadeli@ualberta.ca>
2023-10-04 16:49:53 -0600

    plotted run_8 results moving forward to adjoint operations

--- runs/run_8/Run.py
+++ runs/run_8/Run.py
@@ -10,19 +10,19 @@ guess = {
     'guess_range_imag':[0,100,20]
 }
 
-# Get default_pars and their coresponding solution
-default_pars = obtain_default_pars('pars_list.csv')
-print("Working on default set")
-save_dataframe_to_csv(*find_eig(default_pars, **guess, round_sig_digits=3), 'CSV')
+# # Get default_pars and their coresponding solution
+# default_pars = obtain_default_pars('pars_list.csv')
+# print("Working on default set")
+# save_dataframe_to_csv(*find_eig(default_pars, **guess, round_sig_digits=3), 'CSV')
 
-# Get the solutions for parameter variation
-pars_list = create_custom_pars_list('pars_list.csv')
-for par in pars_list:
-    print("Working on a new custom set")
-    save_dataframe_to_csv(*find_eig(par, default_pars=default_pars, **guess), 'CSV')
+# # Get the solutions for parameter variation
+# pars_list = create_custom_pars_list('pars_list.csv')
+# for par in pars_list:
+#     print("Working on a new custom set")
+#     save_dataframe_to_csv(*find_eig(par, default_pars=default_pars, **guess), 'CSV')
 
-# df = plot_single_df('CSV/default.csv')[0]
-# print(df)
+df = plot_single_df('CSV/default.csv')[0]
+print(df)
 
-# f = [(-300, 100), 400]
-# plot_multiple_datasets('CSV', save_path='multi_plot', rows=4, filter=True, filter_range=f)
+f = [(-300, 100), 400]
+plot_multiple_datasets('CSV', save_path='multi_plot', rows=4, filter=True, filter_range=f)
