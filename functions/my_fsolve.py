5be49461eca3bc1eac67b7d04001ff32ecdb98dc
Behrad 'Brad' Moadeli <moadeli@ualberta.ca>
2023-10-24 22:01:05 -0600

    improved my_fsolve speed added the feature to consider adjoint char_eq

--- functions/my_fsolve.py
+++ functions/my_fsolve.py
@@ -1,49 +1,87 @@
-def my_fsolve(my_fun, m, par, tol_fsolve, tol_is_sol, max_iterations, solution_dict, full_output=True):
+def my_fsolve(m, par, tol_fsolve, tol_is_sol, max_iterations, solution_dict, adj=False, full_output=True):
     import numpy as np
     import pandas as pd
     import scipy.optimize as opt
-    from .create_label import create_label
-    from .obtain_default_pars import obtain_default_pars
+    from .char_eq import char_eq
+    from .char_eq_adj import char_eq_adj
 
+    my_fun = [char_eq, char_eq_adj]
+    if adj:
+        my_fun = [char_eq_adj, char_eq]
+    
     temp_guess = m
     sol = False
     sol_conj = False
-    for _ in range(max_iterations):
-        solution_array, infodict, ier, msg  = opt.fsolve(
-            my_fun, temp_guess, par, xtol=tol_fsolve, full_output=full_output)
-        is_sol = abs(complex(*my_fun(solution_array, par)))
+    iter_counter = 0
+
+    while not sol and iter_counter < max_iterations:
+        
+        iter_counter += 1
+        
+        solution_array, infodict, ier, msg = opt.fsolve(
+            my_fun[0], temp_guess, par, xtol=tol_is_sol, full_output=full_output)
+        is_sol = abs(complex(*my_fun[0](solution_array, par)))
+        
         if np.isclose(is_sol, 0, atol=tol_is_sol):
             sol = True
-            solution_array, infodict, ier, msg = opt.fsolve(
-                my_fun, solution_array, par, xtol=10*tol_is_sol, full_output=full_output)
-            is_sol = abs(complex(*my_fun(solution_array, par)))
-            if is_sol < tol_is_sol:
-                solution_array_conj_guess = solution_array.copy()
-                solution_array_conj_guess[1] *= -1
-                for _ in range(int(np.ceil(np.sqrt(max_iterations)))):
-                    solution_array_conj, infodict_conj, ier_conj, msg_conj = opt.fsolve(
-                        my_fun, solution_array_conj_guess, par, xtol=10*tol_is_sol, full_output=full_output)
-                    # evaluationg the value of my_fun at the obtained relaxed solution
-                    is_sol_conj = abs(complex(*my_fun(solution_array_conj, par)))
-                    sol_conj = True
-                    if np.isclose(is_sol_conj, 0, atol=tol_is_sol):
-                        break
-                    solution_array_conj_guess = solution_array_conj
+            continue
+        
+        solution_array, infodict, ier, msg = opt.fsolve(
+            my_fun[0], solution_array, par, xtol=tol_fsolve, full_output=full_output)
+        is_sol = abs(complex(*my_fun[0](solution_array, par)))
+        
+        if np.isclose(is_sol, 0, atol=tol_is_sol):
+            sol = True
+            continue
+        
         temp_guess = solution_array
-    
+        
+
     if sol:
         solution_dict['Sol_r'].append(solution_array[0])
         solution_dict['Sol_i'].append(solution_array[1])
         solution_dict['Guess'].append(m)
         solution_dict['g(x)'].append(is_sol)
+        solution_dict['g*(x)'].append(abs(complex(*my_fun[1](solution_array, par))))
         solution_dict['ier'].append(ier)
         solution_dict['msg'].append(msg)
         solution_dict['infodict'].append(infodict)
+
+        if abs(solution_array[1]) > tol_fsolve:
+
+            solution_array_conj_guess = solution_array.copy()
+            solution_array_conj_guess[1] *= -1
+
+            iter_counter = 0
+
+            while not sol_conj and iter_counter < np.sqrt(max_iterations):
+                
+                iter_counter += 1
+                
+                solution_array_conj, infodict_conj, ier_conj, msg_conj = opt.fsolve(
+                    my_fun[0], solution_array_conj_guess, par, xtol=tol_is_sol, full_output=full_output)
+                is_sol_conj = abs(complex(*my_fun[0](solution_array_conj, par)))
+
+                if np.isclose(is_sol_conj, 0, atol=tol_is_sol):
+                    sol_conj = True
+                    continue
+
+                solution_array_conj, infodict_conj, ier_conj, msg_conj = opt.fsolve(
+                    my_fun[0], solution_array_conj, par, xtol=tol_fsolve, full_output=full_output)
+                is_sol_conj = abs(complex(*my_fun[0](solution_array_conj, par)))
+
+                if np.isclose(is_sol_conj, 0, atol=tol_is_sol):
+                    sol_conj = True
+                    continue
+
+                solution_array_conj_guess = solution_array_conj
+    
     if sol_conj:
         solution_dict['Sol_r'].append(solution_array_conj[0])
         solution_dict['Sol_i'].append(solution_array_conj[1])
         solution_dict['Guess'].append(solution_array_conj_guess)
         solution_dict['g(x)'].append(is_sol_conj)
+        solution_dict['g*(x)'].append(abs(complex(*my_fun[1](solution_array_conj, par))))
         solution_dict['ier'].append(ier_conj)
         solution_dict['msg'].append(msg_conj)
         solution_dict['infodict'].append(infodict_conj)
